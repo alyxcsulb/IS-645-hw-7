f329288fb1e32071909633a034d3ac00f199b702
Alyx Faber <alyx.faber@student.csulb.edu>
2021-03-23 18:09:58 -0700

    first commit

---
+++ js/ex2.js
@@ -0,0 +1,18 @@
+// Country list
+const countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua-and-Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Autria",
+    "Azerbaïjan"
+  ];
+  
